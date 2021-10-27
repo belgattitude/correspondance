be3eb572cdcb8c8731c59196f3eeb6b5ef054bfd
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-10-27 17:12:14 +0200

    feat: test out stuff

--- apps/web-app/src/features/home/pages/home.page.tsx
+++ apps/web-app/src/features/home/pages/home.page.tsx
@@ -1,13 +1,10 @@
-import { sayHello } from '@your-org/core-lib';
-import { AsyncMessage, Message } from '@your-org/ui-lib';
 import { useTranslation } from 'next-i18next';
 import { NextSeo } from 'next-seo';
-import Image from 'next/image';
 import { DemoApiBlock } from '../blocks/demo-api.block';
 import { homeConfig } from '../home.config';
-import { Banner } from '@/components/banner';
 import { MainLayout } from '@/components/layout/main-layout';
 import { Jumbotron } from '@/features/home/blocks/jumbotron';
+import { Message } from '@/features/home/blocks/message';
 
 type Props = {
   children?: never;
@@ -20,6 +17,7 @@ export const HomePage: React.FC<Props> = () => {
     <>
       <NextSeo title={t('home:page.title')} description="Correspondances" />
       <MainLayout>
+        <Message />
         <Jumbotron />
         <div className={'pt-8'} />
         <DemoApiBlock />
