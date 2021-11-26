13a8bc7deeb8d15614e5512c1ff5347961a2eb61
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-11-26 19:19:54 +0100

    refactor: home

---
+++ apps/web-app/src/pages/home.tsx
@@ -0,0 +1,28 @@
+import { BadRequest } from '@tsed/exceptions';
+import type { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { getServerSideTranslations } from '@/core/i18n/get-server-side-translations';
+import { homeConfig } from '@/features/home/home.config';
+import { HomePage } from '@/features/home/pages/home.page';
+
+type Props = {
+  /** Add HomeRoute props here */
+};
+
+export default function IndexRoute(
+  _props: InferGetStaticPropsType<typeof getStaticProps>
+) {
+  return <HomePage />;
+}
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const { locale } = context;
+  if (locale === undefined) {
+    throw new BadRequest('locale is missing');
+  }
+  const { i18nNamespaces } = homeConfig;
+  return {
+    props: {
+      ...(await getServerSideTranslations(locale, i18nNamespaces)),
+    },
+  };
+};
--- apps/web-app/src/pages/index.tsx
+++ apps/web-app/src/pages/index.tsx
@@ -1,28 +1,64 @@
-import { BadRequest } from '@tsed/exceptions';
-import type { GetStaticProps, InferGetStaticPropsType } from 'next';
-import { getServerSideTranslations } from '@/core/i18n/get-server-side-translations';
-import { homeConfig } from '@/features/home/home.config';
-import { HomePage } from '@/features/home/pages/home.page';
+import {
+  softShadows,
+  MeshWobbleMaterial,
+  MeshDistortMaterial,
+  OrbitControls,
+} from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
 
-type Props = {
-  /** Add HomeRoute props here */
+const canvasStyle: React.CSSProperties = {
+  width: '100vw',
+  height: '100vh',
 };
 
-export default function IndexRoute(
-  _props: InferGetStaticPropsType<typeof getStaticProps>
-) {
-  return <HomePage />;
+softShadows();
+
+const Index = () => {
+  const meshRef = useRef<THREE.Mesh>();
+  useFrame(() => {
+    if (meshRef.current !== undefined) {
+      return (meshRef.current.rotation.x = meshRef.current.rotation.y += 0.01);
+    }
+  });
+  const [video] = useState(() => {
+    const vid = document.createElement('video');
+    //vid.src = 'https://soluble.io/studioworks/mhr/assets/paola-video-bg.mp4';
+    //vid.src = '/videos/paola-video-bg.m4v';
+    vid.src = '/videos/red.mp4';
+    vid.crossOrigin = 'Anonymous';
+    vid.loop = true;
+    vid.playbackRate = 1;
+    vid.muted = true;
+    return vid;
+  });
+  useEffect(() => void video.play(), [video]);
+  return (
+    <mesh ref={meshRef}>
+      <sphereGeometry args={[2, 2]} />
+      <MeshWobbleMaterial color={'white'} speed={1}>
+        <videoTexture attach="map" args={[video]} />
+      </MeshWobbleMaterial>
+    </mesh>
+  );
+
+  /**
+   *       <meshBasicMaterial>
+   <videoTexture attach="map" args={[video]} />
+   </meshBasicMaterial>
+
+   */
+};
+
+export default function VideoRoute() {
+  return (
+    <>
+      <Canvas style={canvasStyle} shadows={true}>
+        <ambientLight intensity={0.4} />
+        <directionalLight color="yellow" position={[0, 5, 5]} />
+        <Index />
+        <OrbitControls />
+      </Canvas>
+    </>
+  );
 }
-
-export const getStaticProps: GetStaticProps<Props> = async (context) => {
-  const { locale } = context;
-  if (locale === undefined) {
-    throw new BadRequest('locale is missing');
-  }
-  const { i18nNamespaces } = homeConfig;
-  return {
-    props: {
-      ...(await getServerSideTranslations(locale, i18nNamespaces)),
-    },
-  };
-};
