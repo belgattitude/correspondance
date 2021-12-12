45b31347c5cb3775720a6a82531d9edb41ca88fa
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-12-12 14:19:34 +0100

    refactor: play

--- apps/web-app/src/themes/tailwind/tailwind.theme.js
+++ apps/web-app/src/themes/tailwind/tailwind.theme.js
@@ -1,10 +1,10 @@
 // @ts-check
 // Keep this file as '.js' as it's included in tailwind.config.js
 
-const { browserFonts } = require('../global/browser-fonts');
+const { browserFonts } = require('../shared/browser-fonts');
 
 module.exports = {
   fontFamily: {
-    sans: ['InterVariable', ...browserFonts.sans],
+    sans: ['Inter', ...browserFonts.sans],
   },
 };
